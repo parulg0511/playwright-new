e7648e60b874a2141acc5d65ac79f228e6bfb8a9
parulg0511 <parulg0511@gmail.com>
2026-03-23 20:22:41 -0700

    Initial commit for new Playwright project

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  use: {
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+});
