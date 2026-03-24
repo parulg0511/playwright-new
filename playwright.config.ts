f6ef77e706deb705353f6d872d77e3f974b47784
parulg0511 <parulg0511@gmail.com>
2026-03-23 23:44:38 -0700

    Remove dotenv, use GitHub secrets

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from '@playwright/test';
-import * as dotenv from 'dotenv';
-dotenv.config();
+
 
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   use: {
     baseURL: process.env.BASE_URL,
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
