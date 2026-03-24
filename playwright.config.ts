8a2ea7b4ffd046d74aec2e29b8aac664496c52bd
parulg0511 <parulg0511@gmail.com>
2026-03-24 08:32:38 -0700

    force update config remove dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from '@playwright/test';
 
-
 export default defineConfig({
   testDir: './tests',
   timeout: 30000,
   use: {
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || 'http://49.249.28.218:8098',
     headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
-  reporter: [['list'], ['html', { outputFolder: 'reports/playwright-report' }]],
+  reporter: [
+    ['list'],
+    ['html', { outputFolder: 'reports/playwright-report' }]
+  ],
 });
